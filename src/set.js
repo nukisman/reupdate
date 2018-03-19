d075ef9dbc9f2b1973b982e3fe824122e90325fd
Alexander Nuikin <nukisman@gmail.com>
2018-03-19 20:48:57 +0300

    set: arrays: Improve performance of some edge cases

--- src/set.js
+++ src/set.js
@@ -9,16 +9,52 @@ const set = (src, target) => {
       switch (srcType) {
         case 'object':
           if (Array.isArray(src) && Array.isArray(target)) {
+            // const diff = src.length - target.length;
+            // if (diff > 0) {
+            //   let changedCount = 0;
+            //   const changedState = [];
+            //   for (let i = 0; i < target.length; i++) {
+            //     const newItem = set(src[i], target[i]);
+            //     changedState.push(newItem);
+            //     if (newItem !== src[i]) changedCount++;
+            //   }
+            //   if (changedCount === target.length) return target;
+            //   else return changedState;
+            // } else if (diff < 0) {
+            //   let changedCount = 0;
+            //   const changedState = [];
+            //   for (let i = 0; i < src.length; i++) {
+            //     const newItem = set(src[i], target[i]);
+            //     changedState.push(newItem);
+            //     if (newItem !== src[i]) changedCount++;
+            //   }
+            //   changedState.push(...target.slice(src.length));
+            //   if (changedCount === target.length) return target;
+            //   else return changedState;
+            // } else {
+            //   let changedCount = 0;
+            //   const changedState = [];
+            //   for (let i = 0; i < target.length; i++) {
+            //     const newItem = set(src[i], target[i]);
+            //     changedState.push(newItem);
+            //     if (newItem !== src[i]) changedCount++;
+            //   }
+            //   if (changedCount === target.length) return target;
+            //   else if (changedCount > 0) return changedState;
+            //   else return src;
+            // }
+
             /** Both arrays */
             let changedCount = 0;
             const changedState = [];
-            for (let i = 0; i < target.length; i++) {
+            const minLen = Math.min(src.length, target.length);
+            for (let i = 0; i < minLen; i++) {
               const newItem = set(src[i], target[i]);
               changedState.push(newItem);
-              if (newItem !== src[i]) {
-                changedCount++;
-              }
+              if (newItem !== src[i]) changedCount++;
             }
+            if (src.length < target.length)
+              changedState.push(...target.slice(minLen));
             if (changedCount === target.length) return target;
             else if (changedCount > 0 || src.length !== target.length)
               return changedState;
